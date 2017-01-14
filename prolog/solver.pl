4b6c34a07f988be4b20e01aa3f118eea1ed4ac75
Cotonne <cotonne@github.com>
2017-01-14 20:16:56 +0100

    A solution

--- prolog/solver.pl
+++ prolog/solver.pl
@@ -11,9 +11,9 @@ getElementAt(X, Y, M, V):-
 	nth0(X, Row, V).
 
 nbOf(_, [], 0).
-nbOf(X, [X|T], N):- 
-	nbOf(X, T, Nx),
-	N is Nx + 1.
+nbOf(X, [X|T], (N+1)):- 
+	nbOf(X, T, N).
+
 nbOf(X, [Y|T], N):-
 	X\==Y, 
 	nbOf(X, T, N).
@@ -28,7 +28,7 @@ getColumns(Y, M, [C|R]) :-
 	getColumn(Y, M, C),
 	Yx is Y + 1,
 	getColumns(Yx, M, R).
-transpose(M, R) :-
+mytranspose(M, R) :-
 	getColumns(0, M, R).
 
 allElementsFrom(Xb, Xe, _, []) :- Xe < Xb.
@@ -51,22 +51,15 @@ replaceWith(V, X, Y, M, Result):-
 
 
 /* Takuzu solver */
-matrixDoesntContainLine(_, []).
-matrixDoesntContainLine(L, [H|T]) :-
-	L \= H,
-	matrixDoesntContainLine(L, T).
+rowDistinct(_, []).
+rowDistinct(R1, [R2|T]) :-
+        R1 \== R2,
+        rowDistinct(R1, T).
 
 matrixDoesntContainDuplicateLines([]).
-matrixDoesntContainDuplicateLines([H|T]) :-
-	nbOf('.', H, N),
-	1 =< N,
+matrixDoesntContainDuplicateLines([R|T]) :-
+        rowDistinct(R, T),
         matrixDoesntContainDuplicateLines(T).
-
-matrixDoesntContainDuplicateLines([H|T]) :- 	
-        nbOf('.', H, N),
-        N = 0,
-	matrixDoesntContainLine(H, T),
-	matrixDoesntContainDuplicateLines(T).
 
 matrixDoesntContainDuplicateLinesOrColumns(M) :- 
 	matrixDoesntContainDuplicateLines(M),
@@ -74,16 +67,30 @@ matrixDoesntContainDuplicateLinesOrColumns(M) :-
 	matrixDoesntContainDuplicateLines(T).
 
 isRowAsFairNumberOf0And1(L) :- 
-	nbOf('0', L, N0),
-	nbOf('1', L, N1),
-	nbOf('.', L, N),
-	abs(N0 - N1) =< N.
+	nbOf(0, L, N0),
+	nbOf(1, L, N1),
+	N0 == N1.
 
+hasNoMoreThan2IdenticalNumbersAdjacent([A, B, _]) :- 
+        A \== B.
+hasNoMoreThan2IdenticalNumbersAdjacent([A, _, B]) :- 
+        A \== B.
+hasNoMoreThan2IdenticalNumbersAdjacent([A, A, A]) :- !, fail.
+hasNoMoreThan2IdenticalNumbersAdjacent([_, _]).
+hasNoMoreThan2IdenticalNumbersAdjacent([_]).
+hasNoMoreThan2IdenticalNumbersAdjacent([]).
+hasNoMoreThan2IdenticalNumbersAdjacent([A, B, C | T]) :-
+        T \= [],
+        hasNoMoreThan2IdenticalNumbersAdjacent([A, B, C]),
+        hasNoMoreThan2IdenticalNumbersAdjacent([B, C|T]).
 
-matrixDoestContainsUnfairNumberOf0or1ForLines([]).
-matrixDoestContainsUnfairNumberOf0or1ForLines([H|T]) :-
-	isRowAsFairNumberOf0And1(H),
- 	matrixDoestContainsUnfairNumberOf0or1ForLines(T).	
+matrixDoesntContainAdjacentDuplicateNumbersInLinesOrColumns(M) :-
+        maplist(hasNoMoreThan2IdenticalNumbersAdjacent, M),
+        transpose(M, T),
+        maplist(hasNoMoreThan2IdenticalNumbersAdjacent, T).
+
+matrixDoestContainsUnfairNumberOf0or1ForLines(T) :-
+        maplist(isRowAsFairNumberOf0And1, T).
 
 matrixDoesntContainUnfairNumberOf0or1ForLinesOrColumns(M) :- 
 	matrixDoestContainsUnfairNumberOf0or1ForLines(M),
@@ -92,40 +99,13 @@ matrixDoesntContainUnfairNumberOf0or1ForLinesOrColumns(M) :-
 
 matrixIsValid(M):- 
 	matrixDoesntContainDuplicateLinesOrColumns(M),
-	matrixDoesntContainUnfairNumberOf0or1ForLinesOrColumns(M).
+	matrixDoesntContainUnfairNumberOf0or1ForLinesOrColumns(M),
+    matrixDoesntContainAdjacentDuplicateNumbersInLinesOrColumns(M).
 
-replaceCurrent(_, Y, M, M) :- 
-	length(M, S),
-	S =< Y.
-replaceCurrent(X, Y, M, R) :-
-        length(M, S),
-	X = S,
-	Yx is Y + 1,
-        replaceCurrent(0, Yx, M, R).
-
-replaceCurrent(X, Y, M, R) :- 
-	getElementAt(X, Y, M, E),
-	E \= '.', 
-	Xx is X + 1,
-	replaceCurrent(Xx, Y, M, R).
-
-replaceCurrent(X, Y, M, R) :-
-        getElementAt(X, Y, M, E),
-        E = '.', 
-	replaceWith('0', X, Y, M, Mx),
-        matrixIsValid(Mx),
-	Xx is X + 1,
-        replaceCurrent(Xx, Y, Mx, R).
-
-replaceCurrent(X, Y, M, R) :-
-        getElementAt(X, Y, M, E),
-        E = '.', 
-        replaceWith('1', X, Y, M, Mx),
- 	matrixIsValid(Mx),
-	Xx is X + 1,
-        replaceCurrent(Xx, Y, Mx, R).
-
-replaceDotBy0or1(M, R) :- replaceCurrent(0, 0, M, R).
+replaceDotBy0or1(M, R) :-
+    append(M, Vs), 
+    Vs ins 0..1,
+    M = R.
 
 solveTakuzu(M, R) :- 
 	replaceDotBy0or1(M, R),
@@ -149,23 +129,11 @@ test(should_replace_the_center_with_0) :-
 	replaceWith(0, 1, 1, [[0, 1, 2], [3, 4, 5], [6, 7, 8]], X),
         X =@= [[0, 1, 2], [3, 0, 5], [6, 7, 8]], !.
 
-test(should_not_find_solution_for_takuzu, [fail]) :-
-	solveTakuzu([['1', '0'], ['1', '0']], _).
-
-test(should_find_a_simple_solution_for_takuzu) :-
-	solveTakuzu([['.', '1'], ['1', '0']], R),
-	R =@= [['0', '1'], ['1', '0']], !.
-	
-test(should_find_another_simple_solution_for_takuzu) :-
-        solveTakuzu([['.', '0'], ['0', '1']], R),
-        R =@= [['1', '0'], ['0', '1']], !.
-
-
 test(should_not_validate_matrix_because_of_unfair_number_of_1, [fail]) :-
-	matrixIsValid([['1', '1'], ['1', '.']]).
+	matrixIsValid([[1, 1], [1, _]]).
 
 test(should_validate_this_matrix) :-
-	matrixIsValid([['1', '0'], ['0', '.']]), !.
+	matrixIsValid([[1, 0], [0, _]]), !.
 
 test(should_get_the_good_terms_from_matrix) :- 
 	getElementAt(2, 1, [[0, 1, 2], [3, 4, 5], [6, 7, 8]], X),
@@ -175,7 +143,7 @@ test(should_not_indicate_that_matrix_contains_duplicate_lines) :-
 	matrixDoesntContainDuplicateLines([[0, 1, 2], [3, 4, 5], [6, 7, 8]]), !.
 
 test(should_not_indicate_that_matrix_contains_duplicate_lines_when_there_is_joker) :-
-        matrixDoesntContainDuplicateLines([['0', '0', '1', '.'], ['0', '0', '1', '.']]), !.
+        matrixDoesntContainDuplicateLines([[0, 0, 1, _], [0, 0, 1, _]]), !.
 
 
 test(should_indicate_that_matrix_contains_duplicate_lines, [fail]) :-
@@ -188,20 +156,18 @@ test(should_say_that_matrix_contains_duplicate_lines_or_columns, [fail]) :-
 	matrixDoesntContainDuplicateLinesOrColumns([[1, 1, 0, 0], [1, 1, 0, 1], [1, 1, 1, 0], [1, 1, 1, 1]]).
 
 test(should_count_the_occurence_of_1) :-
-	nbOf('1', ['1', '0', '.', '1'], X),
+	nbOf(1, [1, 0, _, 1], X),
         X =:= 2, !.
 
 test(should_say_that_row_has_a_fair_number_of_1_and_0):-
-	isRowAsFairNumberOf0And1(['1', '0', '0', '1']), !.
+	isRowAsFairNumberOf0And1([1, 0, 0, 1]), !.
 
 test(should_say_that_row_has_not_a_fair_number_of_1_and_0, [fail]):-
-	isRowAsFairNumberOf0And1(['1', '1', '1', '0']).
+	isRowAsFairNumberOf0And1([1, 1, 1, 0]).
 
-test(should_say_that_row_has_a_fair_number_of_1_and_0_when_there_is_a_joker):-
-	isRowAsFairNumberOf0And1(['1', '0', '.', '1']), !.
 
 test(should_say_that_row_has_not_a_fair_number_of_1_and_0_with_a_joker, [fail]):-
-        isRowAsFairNumberOf0And1(['1', '1', '1', '.']).
+    isRowAsFairNumberOf0And1([1, 1, 1, _]).
 
 
 test(should_return_the_good_column) :- 
@@ -209,8 +175,46 @@ test(should_return_the_good_column) :-
 	C =@= [2, 5, 8], !.
 	
 test(should_transpose_a_matrix) :- 
-	transpose([[0, 1, 2], [3, 4, 5], [6, 7, 8]], X),
+	mytranspose([[0, 1, 2], [3, 4, 5], [6, 7, 8]], X),
         X =@=[[0, 3, 6], [1, 4, 7], [2, 5, 8]], !.
 
+
+test(should_accept_an_empty_line) :-
+        hasNoMoreThan2IdenticalNumbersAdjacent([]).
+
+test(should_accept_when_there_is_only_one_element) :-
+        hasNoMoreThan2IdenticalNumbersAdjacent([0]), !.
+
+test(should_accept_when_there_is_two_elements) :-
+        hasNoMoreThan2IdenticalNumbersAdjacent([0, 1]), !.
+
+test(should_accept_when_there_is_no_more_than_two_identical_adjacent_numbers) :-
+        hasNoMoreThan2IdenticalNumbersAdjacent([0, 0, 1]),
+        hasNoMoreThan2IdenticalNumbersAdjacent([1, 0, 1]),
+        !.
+
+test(should_reject_when_there_is_more_than_two_identical_adjacent_numbers, [fail]) :-
+        hasNoMoreThan2IdenticalNumbersAdjacent([0, 0, 0]),
+        !.
+test(should_accept_a_more_complex_case) :-
+        hasNoMoreThan2IdenticalNumbersAdjacent([0, 0, 1, 1]),
+        hasNoMoreThan2IdenticalNumbersAdjacent([0, 1, 1, 0]),
+        hasNoMoreThan2IdenticalNumbersAdjacent([_, _, _, 0]),
+        !.
+
+test(should_accept_matrix_contains_without_adjacent_duplicate_numbers) :-
+        matrixDoesntContainAdjacentDuplicateNumbersInLinesOrColumns([[1, 1, 0, 0], [1, 1, 0, 1], [0, 0, 1, 0], [0, 1, 1, 0]]), !.
+
+test(should_reject_matrix_with_adjacent_duplicate_numbers_in_column, [fail]) :-
+         matrixDoesntContainAdjacentDuplicateNumbersInLinesOrColumns([[1, 1, 0, 0], [1, 1, 0, 1], [1,     0, 1, 0], [1, 1, 0, 0]]), !.
+
+test(should_not_find_solution_for_takuzu, [fail]) :-
+	solveTakuzu([[1, 0], [1, 0]], _).
+
+test(should_find_a_simple_solution_for_takuzu) :-
+	solveTakuzu([[_, 1], [1, 0]], [[0, 1], [1, 0]]), !.
+	
+test(should_find_another_simple_solution_for_takuzu) :-
+    solveTakuzu([[_, 0], [0, 1]], [[1, 0], [0, 1]]), !.
 
 :- end_tests(takuzu).
